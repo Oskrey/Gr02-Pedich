146c166e99f6fffc3873cbba235ed859d06880cb
Oskrey <91907399+Oskrey@users.noreply.github.com>
2021-12-14 21:55:38 +0300

    Add files via upload

---
+++ extask10-b.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	#define rows 10
+	#define cols 5
+
+	int tab[rows][cols];
+
+	#define mark0 1
+	#define mark1 10
+
+	for(int y = 0; y < rows; y++)
+		for(int x = 0; x < cols; x++)
+			tab[y][x] = mark0 + rand() % (mark1 - mark0 + 1);
+
+	float avg[rows];	
+	for(int y = 0; y < rows; y++)
+	{
+		int s = 0;
+		for(int x = 0; x < cols; x++)
+		{
+			s += tab[y][x];
+		}
+		avg[y] = s / (float)cols;
+	}
+
+    int win = 0 ;
+    float b = avg[0];
+    
+    for(int q = 1; q < rows; q++ )
+    {
+        if (b < avg[q])
+        {
+            b = avg[q];
+            win = q+1;
+        }
+    }
+	
+    printf("num");
+	
+    for(int x = 0; x < cols; x++)
+	{
+		printf("\t%d", x + 1);
+	}
+
+	printf("\tavg\n");
+
+	for(int y = 0; y < rows; y++)
+	{
+		printf("%d", y + 1);
+		for(int x = 0; x < cols; x++)
+		{
+			printf("\t%d", tab[y][x]);
+		}
+		printf("\t%.1f\n", avg[y]);
+	}
+
+    printf("Winner: %d\n", win);
+
+return 0;
+}
